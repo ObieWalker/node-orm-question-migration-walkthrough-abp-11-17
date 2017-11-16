e36f0046d88348d4f4c58ba3b54865a99c2411e5
Obi <obinnawalker@gmail.com>
2017-11-16 19:39:14 +0000

    automatically backed up by learn

--- models/Question.js
+++ models/Question.js
@@ -4,6 +4,22 @@ const db = require("../config/db")
 
 class Question{
 
+  static CreateTable(){
+    return new Promise(function(){
+      const sql =
+      CREATE TABLE questions (
+        id INTEGER PRIMARY KEY,
+        content TEXT
+      )
+
+      db.run(sql, function(){
+        console.log('Question table created!!')
+        resolve('success')
+
+    })
+
+    })
+ }
 }
 
 
